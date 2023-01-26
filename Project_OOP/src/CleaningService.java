5c6df7961bb982fec21beb8f04d6bafffe08445b
Joseph Nazareth Putra <70943613+josephnp@users.noreply.github.com>
2023-01-26 19:01:11 +0700

    Delete Project_OOP directory

--- Project_OOP/src/CleaningService.java
+++
@@ -1,9 +0,0 @@
-
-public class CleaningService extends Employee {
-
-	public CleaningService(String id, String name, String gender, String address, String phoneNumber, int age, int salary) {
-		super(id, name, gender, address, phoneNumber, age, salary);
-	}
-
-
-}
--- Project_OOP/src/Teacher.java
+++
@@ -1,9 +0,0 @@
-
-public class Teacher extends Employee{
-
-	public Teacher(String id, String name, String gender, String address, String phoneNumber, int age, int salary) {
-		super(id, name, gender, address, phoneNumber, age, salary);
-	}
-    
-	
-}
